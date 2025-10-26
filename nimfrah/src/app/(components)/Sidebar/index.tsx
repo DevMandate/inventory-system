19b724a25eff96a97ba346adcb152588b306111d
DevMandate <wafulacaleb233@gmail.com>
2025-10-26 14:56:40 +0300

    Changing the directory structure

---
+++ nimfrah/src/app/(components)/Sidebar/index.tsx
@@ -0,0 +1,77 @@
+"use client"
+
+import { useAppDispatch, useAppSelector } from '@/app/redux'
+import { setIsSidebarCollapsed } from '@/state'
+import { Link, LucideIcon, Menu } from 'lucide-react'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+
+interface SidebarLinkProps {
+  href: string;
+  icon: LucideIcon;
+  label: string;
+  isCollapsed: boolean;
+}
+
+
+const SidebarLink = ({
+  href,
+  icon: Icon,
+  label,
+  isCollapsed
+}: SidebarLinkProps) => {
+  const pathname = usePathname();
+  const isActive = pathname === href || (pathname === '/' && href === '/dashboard');
+
+  return (
+    <Link href={href}>
+      <div className={`cursor-pointe
+        r flex items-center ${isCollapsed ? 'justify-center py-4' : 'justify-start px-8 py-4'} hover:text-blue-500 hover:bg-blue-100 gap-3 transition-colors ${isActive ? 'bg-blue-200 text-white' : ''}`}>
+        
+      </div>
+    </Link>
+  );
+};
+
+
+const Sidebar = () => {
+
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+
+  const sidebarClassNames = `fixed flex flex-col ${isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"} bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`;
+
+
+
+  return ( 
+  <div className={sidebarClassNames}>
+    {/* Top Logo */}
+    <div className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${isSidebarCollapsed ? 'px-5' :'px-8'}`}>
+      <div>logo</div>
+      <h1 className={`${isSidebarCollapsed ? "hidden" : "block"} font-extrabold text-2xl`}>ECOELITES</h1>
+
+      <button className='md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={toggleSidebar}>
+        <Menu className='w-4 h-4' />
+      </button>
+    </div>
+
+    {/* Links */}
+    <div className='flex-grow mt-8'>
+      {/* links here */}
+
+    </div>
+
+    {/* Footer */}
+    <div className='text-center text-xs text-gray-500'>
+      &copy; 2025 EcoElites. All rights reserved.
+    </div>
+  </div>
+  )
+}
+
+export default Sidebar
